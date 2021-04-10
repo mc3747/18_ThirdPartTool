cc4500ca8cbd9e58dc084e555c4007c9d2b5bfa7
小马 <mc3747@126.com>
2021-04-10 10:48:31 +0800

    网易

--- ThirdPartTool/ThirdPartTool/1-国内大厂/9，网易/NeteaseVC.m
+++ ThirdPartTool/ThirdPartTool/1-国内大厂/9，网易/NeteaseVC.m
@@ -19,7 +19,7 @@
     [super viewDidLoad];
     self.title = @"网易";
     self.sectionTitles = @[@"1,开源地址",@"2,OC项目"];
-    self.cellTitles = @[@[@"https://github.com/netease",@"https://github.com/kaola-fed"],@[@"1,"]];
+    self.cellTitles = @[@[@"https://github.com/netease",@"https://github.com/kaola-fed",@"https://github.com/AirtestProject"],@[@"1,AirtestIDE(网易游戏自动化测试框架)"]];
 }
 
 @end
